9f3ba99867f2a83a9305a8d3e32b24efa0704f9a
specoolar <43731654+specoolar@users.noreply.github.com>
2022-08-19 19:13:19 +0500

    Update MonoRoutine.cs

--- MonoRoutine.cs
+++ MonoRoutine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,30 @@ public class MonoRoutine{
     MonoBehaviour context;
     IEnumerator routine;
     IEnumerator processor;
+    Action onStop;
     private bool isRunning = false;
+
+    MultiRoutine group = null;
+    public LinkedListNode<MonoRoutine> GroupNode {
+        get => node;
+    }
+    LinkedListNode<MonoRoutine> node = null;
     public bool IsRunning{
         get => isRunning;
     }
     public MonoRoutine(MonoBehaviour context) => this.context = context;
 
-    public void Start(IEnumerator _routine) {
+    public void AddToGroup(MultiRoutine group, LinkedListNode<MonoRoutine> node) {
+        this.group = group;
+        this.node = node;
+    }
+
+    public void Start(IEnumerator _routine, Action onStop = null) {
         _stop();
+        this.onStop?.Invoke();
         routine = _routine;
         processor = _wait();
+        this.onStop = onStop;
         isRunning = true;
         context.StartCoroutine(_wait());
     }
@@ -24,11 +39,26 @@ public class MonoRoutine{
         processor = null;
         routine = null;
         isRunning = false;
+        UnlinkGroup();
+
+        onStop?.Invoke();
+        onStop = null;
     }
 
     private IEnumerator _wait() {
         yield return routine;
         isRunning = false;
+        UnlinkGroup();
+
+        onStop?.Invoke();
+        onStop = null;
+    }
+
+    void UnlinkGroup() {
+        if (group != null) {
+            group.RemoveRoutine(this);
+            group = null;
+        }
     }
 
     private void _stop() {
